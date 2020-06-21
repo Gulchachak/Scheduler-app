86dac2ad548172113313912ee60b96bf46cb4e41
Gulchachak <ilkaeva85@rambler.ru>
2020-06-21 14:59:26 -0500

    adding database

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zrnzuzolhwrirv:4596caf0441981158a7011c0ea6573e096dba7311c1d806eb2e2cf96e33e0696@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d32i6hub4em3fr'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
